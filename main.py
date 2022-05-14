af2893c887861327a9a3844988c8a3ca5fcca514
Matvey256 <97842332+MatveyKD@users.noreply.github.com>
2022-05-14 16:20:25 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import os
 import argparse
+import logging
+import time
 
 import requests
 from bs4 import BeautifulSoup
@@ -12,13 +14,17 @@ def get_file_type(url):
     return os.path.splitext(urlparse(url).path)[1]
 
 
-def check_for_redirect(url):
-    response = requests.get(url)
+def check_for_redirect(book_id):
+    url = "https://tululu.org/txt.php"
+    response = requests.get(
+        url,
+        params={"id":book_id}
+    )
     response.raise_for_status()
     if response.history:
-        raise requests.HTTPError()
+        raise requests.HTTPError("Page was redirected to Main library page.")
 
-def download_txt(book_id, filename, folder='books/'):
+def download_txt(book_id, filename, folder='books'):
     book_url = f'https://tululu.org/txt.php'
     response_from_loading_book = requests.get(
         book_url,
@@ -26,37 +32,36 @@ def download_txt(book_id, filename, folder='books/'):
     )
     response_from_loading_book.raise_for_status()
 
-    with open(folder+filename, "w", encoding="UTF-8") as file:
+    with open(os.path.join(folder, filename).replace("\\", os.sep), "w", encoding="UTF-8") as file:
         file.write(response_from_loading_book.text)
 
 
 def download_image(book_id, image_url, folder="images/"):
     response = requests.get(image_url)
     response.raise_for_status()
-    with open(f"{folder}{book_id}{get_file_type(image_url)}", "wb") as file:
+    with open(os.path.join(folder, f"{book_id}{get_file_type(image_url)}"), "wb") as file:
         file.write(response.content)
 
 
-def parse_book_page(html_content):
-    soup = BeautifulSoup(html_content.text, 'lxml')
+def parse_book_page(url):
+    response = requests.get(url)
+    response.raise_for_status()
+    
+    soup = BeautifulSoup(response.text, 'lxml')
 
     a = soup.find("div", id="content")
-    print(type(a))
     title_tag = a.find("h1")
     title, author = title_tag.text.split("::")
     title, author = title.strip(), author.strip()
 
-    try:
-        comments_tags = soup.find_all("div", class_="texts")
-        comments = list(map(lambda x: x.find("span").text, comments_tags))
-    except AttributeError:
-        comments = []
+    comments_tags = soup.find_all("div", class_="texts")
+    comments = list(map(lambda x: x.find("span").text, comments_tags))
 
     genres_tags = soup.find("span", class_="d_book").find_all("a")
     genres = list(map(lambda x: x.text, genres_tags))
 
     image_tag = soup.find("div", class_="bookimage").find("a").find("img")
-    image_url = urljoin('https://tululu.org', image_tag["src"])
+    image_url = urljoin(url, image_tag["src"])
 
     return {
         "title": title,
@@ -76,31 +81,38 @@ def main():
     parser.add_argument(
         '--start_id',
         help='От книги какого id скачивать',
+        type=int,
         default=1,
     )
     parser.add_argument(
         '--end_id',
         help='До книги какого id скачивать',
+        type=int,
         default=10
     )
     args = parser.parse_args()
-    
-    for book_id in range(int(args.start_id), int(args.end_id)):
-        print(book_id)
-        try:
-            check_for_redirect(f'https://tululu.org/txt.php?id={book_id}')
-        except requests.HTTPError:
-            pass
-        else:
-            url = f'https://tululu.org/b{book_id}/'
-            response_from_book_page = requests.get(url)
-            response_from_book_page.raise_for_status()
-            book_info = parse_book_page(response_from_book_page)
 
-            filename = f"{book_id}. {book_info['title']}.txt"
+    connection_failed = False
+    for book_id in range(args.start_id, args.end_id):
+        while True:
+            try:
+                check_for_redirect(book_id)
 
-            download_txt(book_id, filename)
-            download_image(book_id, book_info["image_url"])
+                url = f'https://tululu.org/b{book_id}/'
+                book_params = parse_book_page(url)
+
+                filename = f"{book_id}. {sanitize_filename(book_params['title'])}.txt"
+
+                download_txt(book_id, filename)
+                download_image(book_id, book_params["image_url"])
+                break
+            except requests.HTTPError as error:
+                logging.warning(error)
+                break
+            except requests.ConnectionError as error:
+                logging.error(error)
+                time.sleep(3)
+
 
 if __name__ == '__main__':
     main()
